4417d804a4486d0a596510bbfaa6a61b18b449e3
Ram-9040 <padhy0731@gmail.com>
2025-05-25 23:30:19 +0900

    4 files added

---
+++ postpaid.py
@@ -0,0 +1,3 @@
+code for postpaid plan in
+application
+new changes
---
+++ services.py
@@ -0,0 +1,1 @@
+working with the services in Airetl Application[D[D[D[D[D[D[D[D[D[D[D[D[Dtel[C[C[C[C[C[C[C[C[C[C[C[C
