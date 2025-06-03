978fabd6805daea473393d901b886064b6a24cff
Ram-9040 <padhy0731@gmail.com>
2025-06-03 16:21:59 +0900

    feature2 changes

--- Application.java
+++ Application.java
@@ -11,4 +11,32 @@ public class Animal {
     protected void makeSound() {
         System.out.println("Some generic animal sound");
     }
+    #feature2 by ethan on oct 2024 on reports module
+    // File: MyClass.java
+
+// A class with public access specifier
+public class MyClass {
+
+    // Public field
+    public int publicField = 10;
+
+    // Public method
+    public void publicMethod() {
+        System.out.println("This is a public method.");
+    }
+
+    // Main method to run the program
+    public static void main(String[] args) {
+        // Creating an object of MyClass
+        MyClass myObject = new MyClass();
+
+        // Accessing the public field
+        System.out.println("Public Field: " + myObject.publicField);
+
+        // Calling the public method
+        myObject.publicMethod();
+    }
+}
+#end of feature2
+
 }
