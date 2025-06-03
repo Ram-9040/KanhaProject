d8168f0aabff9e4740f073ab5f27c8446fe96adf
Ram-9040 <padhy0731@gmail.com>
2025-06-03 15:09:15 +0900

    added application.java file to qedgedev branch

---
+++ Application.java
@@ -0,0 +1,14 @@
+#Application.java
+// File: Animal.java
+
+// A class with protected access specifier
+public class Animal {
+
+    // Protected field
+    protected String species = "Unknown"; // Initialize with a default value
+
+    // Protected method
+    protected void makeSound() {
+        System.out.println("Some generic animal sound");
+    }
+}
