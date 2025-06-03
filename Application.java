2afe456aaba15318a81ffee47a5fa5e9a705d81a
Ram-9040 <padhy0731@gmail.com>
2025-06-03 16:08:25 +0900

    feature1 changes

--- Application.java
+++ Application.java
@@ -11,4 +11,20 @@ public class Animal {
     protected void makeSound() {
         System.out.println("Some generic animal sound");
     }
+    #feature1 changes by vivek on oct 2024 for services module
+// File: Dog.java
+
+// A subclass of Animal
+public class Dog extends Animal {
+
+    // Public method to access protected members
+    public void displayInfo() {
+        // Accessing the protected field from the superclass
+        System.out.println("Species: " + species);
+
+        // Calling the protected method from the superclass
+        makeSound();
+    }
+}
+#end of feature1 code
 }
